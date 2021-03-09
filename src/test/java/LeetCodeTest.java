7d8e68548a2e3314cde571c43a88b43df8b253c6
wangbaitao <wangbaitao@listener.com.cn>
2021-03-09 16:09:10 +0800

    20210309创建

---
+++ src/test/java/LeetCodeTest.java
@@ -0,0 +1,42 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wangbaitao
+ * @version 1.0.0
+ * <h></h>
+ * @Date 2021/3/9
+ **/
+public class LeetCodeTest {
+    @Test
+    public void test() {
+        int[] nums = {1, 2, 3, 4, 5};
+        int[] result = twoSum(nums, 7);
+        System.out.println("debug");
+    }
+
+    /**
+     * 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 的那两个整数，并返回它们的数组下标。
+     * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+     * 你可以按任意顺序返回答案。
+     * 解题思路：
+     * 标签：哈希映射
+     * 这道题本身如果通过暴力遍历的话也是很容易解决的，时间复杂度在 O(n²)
+     * 由于哈希查找的时间复杂度为 O(1)，所以可以利用哈希容器 map 降低时间复杂度
+     * 遍历数组 nums，i 为当前下标，每个值都判断map中是否存在 target-nums[i] 的 key 值
+     * 如果存在则找到了两个值，如果不存在则将当前的 (nums[i],i) 存入 map 中，继续遍历直到找到为止
+     * 如果最终都没有结果则抛出异常
+     */
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
